3ba5a8290983541692e083ef0e6868ebb4cc7f2b
EFanZh <efanzh@gmail.com>
2018-03-08 21:36:13 +0800

    Add tests for creating controller of fit state

--- src/test/model.test.ts
+++ src/test/model.test.ts
@@ -18,8 +18,8 @@ class FakeView implements model.IViewEventListener {
     }
 }
 
-suite("Model Tests", () => {
-    test("Create Fixed Controller - 100%", () => {
+suite("Model Tests", function () {
+    test("Create Fixed Controller - 100%", function () {
         const fakeView = new FakeView();
 
         model.Controller.createFixed(600, 400, 300, 200, 10, fakeView, 1.1);
@@ -30,7 +30,7 @@ suite("Model Tests", () => {
         assert.equal(fakeView.zoom, 1);
     });
 
-    test("Create Fixed Controller - Corner", () => {
+    test("Create Fixed Controller - Corner", function () {
         const fakeView = new FakeView();
 
         model.Controller.createFixed(600, 400, 580, 380, 10, fakeView, 1.1);
@@ -41,7 +41,7 @@ suite("Model Tests", () => {
         assert.equal(fakeView.zoom, 1);
     });
 
-    test("Create Fixed Controller - Fit Horizontal", () => {
+    test("Create Fixed Controller - Fit Horizontal", function () {
         const fakeView = new FakeView();
 
         model.Controller.createFixed(600, 400, 581, 380, 10, fakeView, 1.1);
@@ -54,7 +54,7 @@ suite("Model Tests", () => {
         assert.equal(fakeView.zoom, expectedZoom);
     });
 
-    test("Create Fixed Controller - Fit Vertical", () => {
+    test("Create Fixed Controller - Fit Vertical", function () {
         const fakeView = new FakeView();
 
         model.Controller.createFixed(600, 400, 580, 381, 10, fakeView, 1.1);
@@ -63,6 +63,58 @@ suite("Model Tests", () => {
 
         assert.equal(fakeView.zoomMode, model.ZoomMode.Fixed);
         assert.equal(fakeView.x, (600 - 580 * expectedZoom) / 2);
+        assert.equal(fakeView.y, 10);
+        assert.equal(fakeView.zoom, expectedZoom);
+    });
+
+    test("Create Fit Controller - Upscaling - Fit Horizontal", function () {
+        const fakeView = new FakeView();
+
+        model.Controller.createFit(600, 400, 100, 10, 10, fakeView, 1.1);
+
+        const expectedZoom = 5.8;
+
+        assert.equal(fakeView.zoomMode, model.ZoomMode.Fit);
+        assert.equal(fakeView.x, 10);
+        assert.equal(fakeView.y, (400 - 10 * expectedZoom) / 2);
+        assert.equal(fakeView.zoom, expectedZoom);
+    });
+
+    test("Create Fit Controller - Upscaling - Fit Vertical", function () {
+        const fakeView = new FakeView();
+
+        model.Controller.createFit(600, 400, 10, 100, 10, fakeView, 1.1);
+
+        const expectedZoom = 3.8;
+
+        assert.equal(fakeView.zoomMode, model.ZoomMode.Fit);
+        assert.equal(fakeView.x, (600 - 10 * expectedZoom) / 2);
+        assert.equal(fakeView.y, 10);
+        assert.equal(fakeView.zoom, expectedZoom);
+    });
+
+    test("Create Fit Controller - Downscaling - Fit Horizontal", function () {
+        const fakeView = new FakeView();
+
+        model.Controller.createFit(600, 400, 1000, 100, 10, fakeView, 1.1);
+
+        const expectedZoom = 0.58;
+
+        assert.equal(fakeView.zoomMode, model.ZoomMode.Fit);
+        assert.equal(fakeView.x, 10);
+        assert.equal(fakeView.y, (400 - 100 * expectedZoom) / 2);
+        assert.equal(fakeView.zoom, expectedZoom);
+    });
+
+    test("Create Fit Controller - Downscaling - Fit Vertical", function () {
+        const fakeView = new FakeView();
+
+        model.Controller.createFit(600, 400, 100, 1000, 10, fakeView, 1.1);
+
+        const expectedZoom = 0.38;
+
+        assert.equal(fakeView.zoomMode, model.ZoomMode.Fit);
+        assert.equal(fakeView.x, (600 - 100 * expectedZoom) / 2);
         assert.equal(fakeView.y, 10);
         assert.equal(fakeView.zoom, expectedZoom);
     });
